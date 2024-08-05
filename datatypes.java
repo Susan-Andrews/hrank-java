157c6b2fb1a79c2d3743023e9791f2d1b3ee0ab5
Susan Andrews <susanandrews2001@gmail.com>
2024-08-05 22:15:35 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,6 @@
+primitive 
+  byte, short, int, long, float, double, boolean and char
+non-primitive
+     String, Arrays and Classes 
+each class , ie, Byte,Short,Integer,Long etc have their range specified in the variables or functions called  .MIN_VALUE , .MAX_VALUE by them we can specify tghe range without automaticcaly learning it.
+add the conditions in the if , so everyone will be checked 
