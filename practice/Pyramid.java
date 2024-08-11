e35ead11764e7b6829b03f91b09a051679ecb583
Susan Andrews <susanandrews2001@gmail.com>
2024-08-11 23:05:38 +0530

    pyramid2

--- practice/Pyramid.java
+++ practice/Pyramid.java
@@ -1,22 +1,22 @@
 // Right Half Pyramid Pattern  
 
-// class pyramid{
-//     public static void main(String args[]){
-//         int rows=5;
-//         for(int i=1;i<=rows;i++){
-//             for (int j=1;j<=i;j++){
-//                 System.out.print("* ");
-//             }
-//             System.out.println();
-//         }
-//     }
-// }
+class pyramid{
+    public static void main(String args[]){
+        int rows=5;
+        for(int i=1;i<=rows;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
 
-// * 
-// **
-// ***
-// ****
-// *****
+* 
+**
+***
+****
+*****
 
 // Left Half Pyramid Pattern 
 
@@ -40,8 +40,64 @@ class pyramid{
     }
 }
 
-//     *
-//    **
-//   ***
-//  ****
-// *****
+    *
+   **
+  ***
+ ****
+*****
+
+
+// Inverted Right Half Pyramid Pattern 
+
+class pyramid{
+    public static void main(String args[]){
+        int rows=5;
+        //to print rows
+        for(int i=rows;i>=1;i--){
+        //to print stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+
+*****
+**** 
+***  
+**   
+* 
+
+
+// Inverted Left Half Pyramid Pattern 
+
+class pyramid{
+    public static void main(String args[]){
+        int rows=5;
+        //to print rows
+        for(int i=rows;i>=1;i--){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            //to print stars
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+
+
+            
+        }
+            
+    }
+}
+
+*****
+ ****
+  ***
+   **
+    *
